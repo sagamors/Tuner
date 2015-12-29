b418400942d29a906bc6ad384b0c2c185d84c4e4
sagamors <sagamor@inbox.ru>
2015-12-29 20:37:18 +0300

    add realization

--- Tuner.Wpf/Constrols/Sector.cs
+++ Tuner.Wpf/Constrols/Sector.cs
@@ -11,23 +11,65 @@ namespace Tuner.Wpf.Constrols
 {
     public class Sector : Shape
     {
-        public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register(
-            "Radius", typeof (double), typeof (Sector), new FrameworkPropertyMetadata(default(double),FrameworkPropertyMetadataOptions.AffectsRender));
-
         public double Radius
         {
-            get { return (double) GetValue(RadiusProperty); }
-            set { SetValue(RadiusProperty, value); }
-        }
+            get
+            {
+                double radius = CorrectedWidth / 2;
 
+                if (CorrectedWidth > CorrectedHeight)
+                {
+                    radius = CorrectedHeight / 2;
+                }
+                return radius;
+            }
+        }
         private double CorrectedWidth
         {
-            get { return ActualWidth - StrokeThickness/2; }
+            get { return ActualWidth - StrokeThickness / 2; }
         }
 
         private double CorrectedHeight
         {
-            get { return ActualHeight - StrokeThickness/2; }
+            get { return ActualHeight - StrokeThickness / 2; }
+        }
+
+        public static readonly DependencyProperty AngleProperty = DependencyProperty.Register(
+            "Angle", typeof (double), typeof (Sector),
+            new FrameworkPropertyMetadata(45.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public double Angle
+        {
+            get { return (double) GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        public static readonly DependencyProperty StartAngleProperty = DependencyProperty.Register(
+            "StartAngle", typeof (double), typeof (Sector),
+            new FrameworkPropertyMetadata(default(double), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public double StartAngle
+        {
+            get { return (double) GetValue(StartAngleProperty); }
+            set { SetValue(StartAngleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SweepDirectionProperty = DependencyProperty.Register(
+                "SweepDirection", typeof(SweepDirection), typeof(Sector), new FrameworkPropertyMetadata(default(SweepDirection), FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public SweepDirection SweepDirection
+        {
+            get { return (SweepDirection) GetValue(SweepDirectionProperty); }
+            set { SetValue(SweepDirectionProperty, value); }
+        }
+
+        public static readonly DependencyProperty ThicknessProperty = DependencyProperty.Register(
+            "Thickness", typeof (double), typeof (Sector), new FrameworkPropertyMetadata(default(double),FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public double Thickness
+        {
+            get { return (double) GetValue(ThicknessProperty); }
+            set { SetValue(ThicknessProperty, value); }
         }
 
         protected override Geometry DefiningGeometry
@@ -44,11 +86,27 @@ namespace Tuner.Wpf.Constrols
                     return geometry;
                 }
 
+                var center = new Point(CorrectedWidth / 2, CorrectedHeight / 2);
+                double startAngleInRads = MathHelper.ConvertToRads(StartAngle);
+                double endAngle = (StartAngle + Angle);
+                double endAngleInRads = MathHelper.ConvertToRads(endAngle);
+                int direction = (SweepDirection == SweepDirection.Clockwise ? 1 : -1);
+                var beginVector = new Vector(Math.Cos(startAngleInRads), Math.Sin(startAngleInRads) * direction);
+                //lower left point
+                Point beginLeftPoint = center + beginVector * (Radius - Thickness);
+                //bottom right point
+                Point beginningRightPoint = center + beginVector * Radius;
+                var endVector = new Vector(Math.Cos(endAngleInRads), Math.Sin(endAngleInRads) * direction);
+                Point endRightPoint = center + endVector * Radius;
+                var smallRadius = Radius - Thickness;
+                Point endLeftPoint = center + endVector * smallRadius;
                 using (StreamGeometryContext context = geometry.Open())
                 {
-                    context.BeginFigure(new Point(0, this.CorrectedHeight / 2),true,true);
-                    context.LineTo(new Point(CorrectedWidth - Radius,0),true,true );
-                    context.ArcTo(new Point(CorrectedWidth - Radius, CorrectedHeight),new Size(Radius, CorrectedHeight),0,false,SweepDirection.Clockwise, true,true);
+                    context.BeginFigure(beginLeftPoint,true,true);
+                    context.LineTo(beginningRightPoint,true,true );
+                    context.ArcTo(endRightPoint,new Size(Radius , Radius),0,false, SweepDirection, true,true);
+                    context.LineTo(endLeftPoint, true, true);
+                    context.ArcTo(beginLeftPoint, new Size(smallRadius, smallRadius), 0, false, (SweepDirection == SweepDirection.Clockwise ? SweepDirection.Counterclockwise : SweepDirection.Clockwise), true, true);
                 }
 
                 // Freeze the geometry for performance benefits
