5a444224191074e6d911e24af903c702270d1102
sagamors <sagamor@inbox.ru>
2016-02-01 21:32:51 +0300

    add MainViewModel

---
+++ Tuner.Wpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Tuner.Wpf.Helpers;
+
+namespace Tuner.Wpf.ViewModels
+{
+    class MainViewModel
+    {
+        public ICommand OpenSettingsCommand {private set; get; }
+
+        public MainViewModel()
+        {
+            OpenSettingsCommand = new RelayCommand(OpenSettings);
+        }
+
+        public void OpenSettings()
+        {
+
+        }
+    }
+}
