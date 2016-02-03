9bdbdfd65d0b16b3f9e8be572de36aca210386b4
sagamors <sagamor@inbox.ru>
2016-02-03 22:24:25 +0300

    add parameters to Settings

--- Tuner.Wpf/ViewModels/ISettingsViewModel.cs
+++ Tuner.Wpf/ViewModels/ISettingsViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,10 @@ namespace Tuner.Wpf.ViewModels
 {
     interface ISettingsViewModel : IViewModel<ISettingsView>
     {
-        
+        ObservableCollection<MMDevice> Devices { set; get; }
+        MMDevice SelectedDevice { set; get; }
+        int SampleRate { set; get; }
+        int BitDepth { set; get; }
+        int ChannelCount { set; get; }
     }
 }
--- Tuner.Wpf/ViewModels/SettingsViewModel.cs
+++ Tuner.Wpf/ViewModels/SettingsViewModel.cs
@@ -1,10 +1,18 @@
-﻿using Ninject;
+﻿using System;
+using System.Collections.ObjectModel;
+using NAudio.CoreAudioApi;
+using Ninject;
 using Tuner.Wpf.Core;
 
 namespace Tuner.Wpf.ViewModels
 {
     public class SettingsViewModel : ViewModelBase<ISettingsView>, ISettingsViewModel
     {
+        public int BitDepth { set; get; }
+        public int ChannelCount { set; get; }
+        public int SampleRate { set; get; }
+        public ObservableCollection<MMDevice> Devices { set; get; }
+        public MMDevice SelectedDevice   { set; get; }
         public SettingsViewModel(IKernel kernel) : base(kernel)
         { 
 
