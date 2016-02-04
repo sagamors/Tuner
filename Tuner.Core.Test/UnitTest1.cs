02b99fb4ce90a87342e368354651318733018336
sagamors <sagamor@inbox.ru>
2016-02-04 19:44:00 +0300

    added classes for analysis and edited settings

---
+++ Tuner.Core.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tuner.Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NoteFinderTest()
+        {
+            NoteFactory noteFactory = new NoteFactory();
+            NoteFinder noteFinder = new NoteFinder(noteFactory);
+            Note note = new Note("C",2,110);
+            var actual = noteFinder.FindNearestNote(110);
+            var v = actual == note;
+            Assert.AreEqual(actual, note);
+        }
+    }
+}
